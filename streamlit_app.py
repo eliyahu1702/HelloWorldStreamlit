b55c659a3aa035bb0ed4463fd2cad4dc53d89b61
eliyahu1702 <eliyahu531@gmail.com>
2023-07-28 15:10:52 +0300

    Create streamlit_app.py
    learning the basics with streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+dict = {"a": "alpha", "o": "omega", "g": "gamma"}
+
+st.header("St Button Time")
+button = st.button(label="hello button")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+}))
+st.write(dict)
+st.write('Hello, *World!* :sunglasses:')
+df = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
+
+c = alt.Chart(df).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
+
