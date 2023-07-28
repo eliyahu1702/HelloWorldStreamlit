ece63aed96d6688be94d3e2b733384407e5ba4cd
eliyahu1702 <eliyahu531@gmail.com>
2023-07-28 16:33:31 +0300

    slide bar go brrrrrrrrrr
    slide bar test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,13 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
+
 dict = {"a": "alpha", "o": "omega", "g": "gamma"}
-
-st.header("St Button Time")
-button = st.button(label="hello button")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40],
-}))
-st.write(dict)
-st.write('Hello, *World!* :sunglasses:')
-df = pd.DataFrame(
-    np.random.randn(200, 4),
-    columns=['a', 'b', 'c','d'])
-
-c = alt.Chart(df).mark_circle().encode(
-    x='a', y='b', size='c', color='d', tooltip=['a', 'b', 'c'])
-st.write(c)
+st.header("slider time")
+answer =  st.slider("what's 9 + 10?",min_value=0, max_value=100)
+chart_data = pd.read_excel("Book.xlsx",index_col="Time")
+new_chart = chart_data.head(answer)
+st.line_chart(new_chart)
+if answer == 21:
+    st.header("You Stupid")
 
