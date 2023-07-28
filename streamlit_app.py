971d04fada6460ff83e7ac74e070a53001afb220
eliyahu1702 <eliyahu531@gmail.com>
2023-07-28 16:59:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import altair as alt
 dict = {"a": "alpha", "o": "omega", "g": "gamma"}
 st.header("slider time")
 answer =  st.slider("what's 9 + 10?",min_value=0, max_value=100)
-chart_data = pd.read_excel("Book.xlsx",index_col="Time")
+chart_data = pd.read_csv("Book.csv",index_col="Time")
 new_chart = chart_data.head(answer)
 st.line_chart(new_chart)
 if answer == 21:
